c77708c5b7f151a826084a0ede92bc7972070477
Karan-Singh-Bhandari-19 <karanbhandari037@gmail.com>
2025-03-21 21:50:42 +0530

    added x icon

--- frontend/Admin/src/lib/axios.js
+++ frontend/Admin/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5173' || 'https://emart-2srq.onrender.com',
+    baseURL: 'https://emart-2srq.onrender.com',
     withCredentials: true,
 });
