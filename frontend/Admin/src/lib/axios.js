4888d75c7f744ce536813fca3f3b2f38527de07b
Karan-Singh-Bhandari-19 <karanbhandari037@gmail.com>
2025-03-21 19:30:28 +0530

    updated url

--- frontend/Admin/src/lib/axios.js
+++ frontend/Admin/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:  import.meta.env.VITE_BACKEND_URL || 'https://emart-2srq.onrender.com',
+    baseURL: 'https://emart-2srq.onrender.com',
     withCredentials: true,
 });
